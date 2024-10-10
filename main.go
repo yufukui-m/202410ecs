4bf439f99e432f6bb9924b0d3912808cc6727e32
dummy <pprmz5+yufukui-m@gmail.com>
2024-10-11 07:19:50 +0900

    add bingo

--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World")
+	})
+
+	r.GET("/bingo", func(c *gin.Context) {
+		n := rand.Intn(74) + 1
+		c.String(http.StatusOK, strconv.Itoa(n))
 	})
 
 	r.GET("/hello/:name", func(c *gin.Context) {
